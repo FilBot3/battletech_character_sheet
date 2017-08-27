37348699fe36e1a6306284f5da14be7439dfe7b5
Phillip Dudley <Predatorian3@gmail.com>
2017-08-27 07:39:31 -0500

    Changed the method in which watcher works. its now a system() call.

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,18 +3,17 @@
 require 'rubygems'
 require 'bundler'
 require 'bundler/setup'
-require 'guard'
-require 'guard/commander'
 
 desc 'Generate the index.html from character_sheet.rb'
 task :create do 
-        puts '=> Creating index.html'
-        sh 'ruby character_sheet.rb'
+  puts '=> Creating index.html'
+  sh 'ruby character_sheet.rb'
 end
 
 task :default => ['create']
 
 desc 'Watch with Guard, all tasks'
 task :watcher do 
-        Guard.setup(guardfile: 'Guardfile')
+  puts '=> Watching with Guard'
+  Kernel.system('bundle exec guard')
 end
